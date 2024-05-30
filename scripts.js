a7746cbf782c3c6b88fad367006853c648aa6102
dylan-barnes <59466084+dylan-barnes@users.noreply.github.com>
2024-05-30 18:33:50 +0930

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,26 @@
+const mainTitle = document.querySelector('#title');
+let curValue = 0;
+
+const btnDecrement = document.querySelector('#decrement');
+const btnReset = document.querySelector('#reset');
+const btnIncrement = document.querySelector('#increment');
+
+btnIncrement.addEventListener('click', () => {
+  curValue++;
+  if (curValue == 11){
+    curValue = 0;
+  }
+  mainTitle.textContent = "Points: " + String(curValue);
+});
+
+btnDecrement.addEventListener('click', () => {
+  curValue--;
+  if (curValue == -1){
+    curValue = 0;
+  }
+  mainTitle.textContent = "Points: " + String(curValue);
+});
+
+btnReset.addEventListener('click', () => {
+  window.location.href = './index.html'
+});
